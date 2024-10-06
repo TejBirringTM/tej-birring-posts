b2225f6471730da667a03424761e8055fef1e30b
Tej Birring <tejbirring@gmail.com>
2024-10-06 03:04:50 +0100

    Add link tag for Favicon.ico to the main layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/x-icon" href="/assets/favicon.ico"></link>
+      </head>
       <body>
         <TheHeader />
           <Provider>
